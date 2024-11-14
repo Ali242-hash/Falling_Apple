e6749bab3f0a55f1f185810a1d5d2ed86a6654f9
Ebtekar.Ali <Ebtekar.Ali@C19-05>
2024-11-14 21:02:52 +0100

    FInal Versin of my code

--- Falling_Apple/Form1.Designer.cs
+++ Falling_Apple/Form1.Designer.cs
@@ -29,13 +29,132 @@ namespace Falling_Apple
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Tree = new System.Windows.Forms.PictureBox();
+            this.Tree1 = new System.Windows.Forms.PictureBox();
+            this.Head = new System.Windows.Forms.PictureBox();
+            this.Body = new System.Windows.Forms.PictureBox();
+            this.Hand = new System.Windows.Forms.PictureBox();
+            this.Basket = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Tree)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tree1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Head)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Body)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Hand)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Basket)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Tree
+            // 
+            this.Tree.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Tree.Location = new System.Drawing.Point(12, 12);
+            this.Tree.Name = "Tree";
+            this.Tree.Size = new System.Drawing.Size(194, 120);
+            this.Tree.TabIndex = 0;
+            this.Tree.TabStop = false;
+            // 
+            // Tree1
+            // 
+            this.Tree1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Tree1.Location = new System.Drawing.Point(77, 93);
+            this.Tree1.Name = "Tree1";
+            this.Tree1.Size = new System.Drawing.Size(53, 355);
+            this.Tree1.TabIndex = 1;
+            this.Tree1.TabStop = false;
+            // 
+            // Head
+            // 
+            this.Head.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Head.Location = new System.Drawing.Point(369, 300);
+            this.Head.Name = "Head";
+            this.Head.Size = new System.Drawing.Size(52, 51);
+            this.Head.TabIndex = 2;
+            this.Head.TabStop = false;
+            // 
+            // Body
+            // 
+            this.Body.BackColor = System.Drawing.Color.Chocolate;
+            this.Body.Location = new System.Drawing.Point(385, 347);
+            this.Body.Name = "Body";
+            this.Body.Size = new System.Drawing.Size(36, 101);
+            this.Body.TabIndex = 3;
+            this.Body.TabStop = false;
+            // 
+            // Hand
+            // 
+            this.Hand.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Hand.Location = new System.Drawing.Point(329, 372);
+            this.Hand.Name = "Hand";
+            this.Hand.Size = new System.Drawing.Size(64, 32);
+            this.Hand.TabIndex = 4;
+            this.Hand.TabStop = false;
+            // 
+            // Basket
+            // 
+            this.Basket.BackColor = System.Drawing.Color.Red;
+            this.Basket.Location = new System.Drawing.Point(619, 372);
+            this.Basket.Name = "Basket";
+            this.Basket.Size = new System.Drawing.Size(137, 76);
+            this.Basket.TabIndex = 5;
+            this.Basket.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Red;
+            this.pictureBox3.Location = new System.Drawing.Point(659, 326);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(54, 50);
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(385, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Basket);
+            this.Controls.Add(this.Head);
+            this.Controls.Add(this.Tree);
+            this.Controls.Add(this.Tree1);
+            this.Controls.Add(this.Body);
+            this.Controls.Add(this.Hand);
+            this.Controls.Add(this.pictureBox3);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Tree)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tree1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Head)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Body)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Hand)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Basket)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Tree;
+        private System.Windows.Forms.PictureBox Tree1;
+        private System.Windows.Forms.PictureBox Head;
+        private System.Windows.Forms.PictureBox Body;
+        private System.Windows.Forms.PictureBox Hand;
+        private System.Windows.Forms.PictureBox Basket;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Falling_Apple/Form1.cs
+++ Falling_Apple/Form1.cs
@@ -12,9 +12,209 @@ namespace Falling_Apple
 {
     public partial class Form1 : Form
     {
+        Timer AppleFallTimer = new Timer();
+        Timer HandMoveTimer = new Timer();
+        Timer AppleSpwanTimer = new Timer();
+        Timer PersonMoveTimer = new Timer();
+
+        List<PictureBox> Apples = new List<PictureBox>();
+        PictureBox AppleHeldByHand = null;
+
+        bool MovingLeft = true;
+        bool MovingBasket = false;
+        bool IncreaseSpeed = false;
+        bool IncreaseBask = false;
+
+        int HitByHand =0;
+        int BasketCapa = 50;
+
+        int point = 0;
+        Random rnd = new Random();
+   
+      
+
         public Form1()
         {
             InitializeComponent();
+            Start();
+            
         }
+
+        void Start()
+        {
+            SetUpTime();
+            AddApple();
+            UpdatePoint();
+            label1.Font = new Font(label1.Font.FontFamily, 20,FontStyle.Bold);
+            
+        }
+
+        void SetUpTime()
+        {
+            AppleFallTimer.Interval = 100;
+            HandMoveTimer.Interval = 20;
+            AppleSpwanTimer.Interval = RandomSpeed();
+         
+
+            AppleFallTimer.Start();
+            HandMoveTimer.Start();
+            AppleSpwanTimer.Start();
+            
+
+            AppleFallTimer.Tick += AppleFallEvent;
+            HandMoveTimer.Tick += HandMoveEvent;
+            AppleSpwanTimer.Tick += (s, e) => AddApple();
+           
+
+        
+
+        }
+
+        void AppleFallEvent(object s,EventArgs e)
+        {
+          if(AppleHeldByHand == null)
+            {
+                for(int i = 0; i < Apples.Count; i++)
+                {
+                    PictureBox OneApple = Apples[i];
+
+                    if (OneApple.Visible)
+                        OneApple.Top += 2;
+
+                    if (i > 0 && Apples[i - 1].Bottom + 20 <= OneApple.Top)
+                        Apples[i - 1].Top += 2;
+
+                    if (OneApple.Bounds.IntersectsWith(Hand.Bounds))
+                    {
+                        OneApple.Hide();
+                        AppleHeldByHand = OneApple;
+                        MovingBasket = true;
+                        return;
+                    }
+
+                    if (OneApple.Top > this.ClientSize.Height)
+                    {
+                        this.Controls.Remove(OneApple);
+                        Apples.Remove(OneApple);
+                        i--;
+
+                    }
+                }
+            }
+
+        
+        }
+
+
+        void HandMoveEvent(object s,EventArgs e)
+        {
+
+                if (Hand.Bounds.IntersectsWith(Tree1.Bounds))
+                {
+                    HitByHand++;
+                    if (HitByHand >= 10)
+                    {
+                        HitByHand = 0;
+                        AddApple();
+                    }
+                }
+
+          
+           
+            if (MovingLeft)
+            {
+                Hand.Left -= 3;
+                Head.Left -= 3;
+                Body.Left -= 3;
+
+                if(Hand.Left < 0)
+                {
+                    MovingLeft = false;
+                }
+            }
+          
+            else
+            {
+                Hand.Left += 3;
+                Head.Left += 3;
+                Body.Left += 3;
+
+                int screen = 70;
+
+                if(Hand.Left > this.ClientSize.Width -Hand.Width - screen)
+                {
+                    MovingLeft = true;
+                }
+
+                else
+                {
+                    DropeAppleToBasket();
+                    MovingBasket = false;
+                }
+            }
+            
+
+           
+
+
+        }
+
+        void AddApple()
+        {
+            PictureBox OneApple = new PictureBox();
+            OneApple.Width = 15;
+            OneApple.Height = 15;
+            OneApple.BackColor = Color.Red;
+            OneApple.Visible = true;
+            OneApple.Top = (Tree.Height + 30);
+            OneApple.Left = (Tree.Width + 30);
+            this.Controls.Add(OneApple);
+            Apples.Add(OneApple);
+
+        }
+
+        void DropeAppleToBasket()
+        {
+            if (AppleHeldByHand != null)
+            {
+                this.Controls.Remove(AppleHeldByHand);
+                AppleHeldByHand = null;
+                Apples.Remove(AppleHeldByHand);
+                point++;
+
+                if(point >=5 && point %5==0 && !IncreaseBask)
+                {
+                    BasketCapa += 5;
+                    Basket.Width += 5;
+                    point += 2;
+                    IncreaseBask = true;
+                }
+
+                if(point>=30 && !IncreaseSpeed)
+                {
+                    if (AppleFallTimer.Interval > 1) AppleFallTimer.Interval--;
+                    if (HandMoveTimer.Interval > 1) HandMoveTimer.Interval--;
+                    IncreaseSpeed = true;
+                }
+
+                UpdatePoint();
+                   
+
+            }
+
+        }
+
+        int RandomSpeed()
+        {
+            return rnd.Next(1500, 2501);
+        }
+
+
+        void UpdatePoint()
+        {
+            label1.Text = $"Your point {point}";
+        }
+
+     
     }
 }
